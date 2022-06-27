f90295d9cec726db9c10d658072c5afa028fa2dd
Raushan Kumar <raushansft@gmail.com>
2022-06-27 21:17:08 +0530

    structure

---
+++ src/components/Home/Favorite.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Favorite() {
+  return (
+    <>
+    <h1>Favorite item</h1>
+      
+    </>
+  )
+}
---
+++ src/components/Home/Quick.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Quick() {
+  return (
+    <>
+    <h1>Quick item</h1>
+      
+    </>
+  )
+}
